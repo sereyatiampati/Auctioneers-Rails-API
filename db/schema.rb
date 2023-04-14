285c0b522409b7946ebb01dee199f5b5443925bf
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-14 13:52:02 +0300

    Generated bids Model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_101429) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_105037) do
+  create_table "bids", force: :cascade do |t|
+    t.float "bid_amount"
+    t.datetime "bid_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
