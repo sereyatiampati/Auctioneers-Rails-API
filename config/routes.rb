94de696b385d570e1221ea9a9be77ff0b2f1a5ac
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-14 14:29:38 +0300

    Added routes for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,31 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  ##USERS
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  delete '/users', to: 'users#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  ##PRODUCTS
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show'
+  post '/products', to: 'products#create'
+  delete 'products/:id', to: 'products#destroy'
+
+  ##BIDS
+  get '/bids', to: 'bids#index'
+  get '/bids/:id', to: 'bids#show'
+  post '/bids', to: 'bids#create'
+  delete '/bids', to: 'bids#destroy'
+
+  ##CATEGORIES
+  get '/categories', to: 'categories#index'
+  get '/categories/:id', to: 'categories#show'
+  post '/categories', to: 'categories#create'
+  delete '/categories/:id', to: 'categories#destroy'
+
+  ##ROLES
+  get '/roles', to: 'roles#index'
+  get '/roles/:id', to: 'roles#show'
+  post '/roles', to: 'roles#create'
+  delete '/roles/:id', to: 'roles#destroy'
 end
