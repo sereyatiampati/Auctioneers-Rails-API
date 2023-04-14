ffc3a6852a16ed2f6ba897c540300b4d75fe7470
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-14 13:56:48 +0300

    Added methods to bids controller

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,41 @@
 class BidsController < ApplicationController
+
+    def index
+        render json: Bid.all
+    end
+
+    def create
+        bid = Bid.create(bid_params)
+        if bid
+            render json: bid, status: :created
+        else
+            render json: {error: "Bid not created"}, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        bid = Bid.find_by(id: params[:id])
+        if bid
+            render json: bid, status: :ok
+        else
+            render json: {error: "Bid not found"}, status: :not_found
+        end
+    end
+
+    def destroy
+        bid = Bid.find_by(id: params[:id])
+        if bid
+            bid.destroy
+            head :no_content
+        else
+            render json: {error: "Bid not found"}, status: :not_found
+        end
+    end
+
+    private
+    def bid_params
+        params.permit(:bid_amount, :bid_time)
+    end
+    
+
 end
