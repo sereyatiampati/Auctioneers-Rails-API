9038e408d3a6c9195a79f31a3ededff747e0f26d
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-04-14 13:34:16 +0300

    Added methods to roles controller

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,2 +1,41 @@
 class RolesController < ApplicationController
+
+    def index
+        render json: Role.all
+    end
+
+    def show
+        role = Role.find_by(id: params[:id])
+        if role
+            render json: role, status: :ok
+        else
+            render json: {error:"Role not found"}, status: :not_found
+        end
+    end
+
+    def create 
+        role = Role.create(role_params)
+        if role
+            render json: role, status: :created
+        else
+            render json: {error: "Role not created"}, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        role = Role.find_by(id: params[:id])
+        if role
+            role.destroy
+            head :no_content
+        else
+            render json: {error: "Role not found"}, status: :not_found
+        end
+    end
+
+    private
+
+    def role_params
+        params.permit(:name)
+    end
+
 end
