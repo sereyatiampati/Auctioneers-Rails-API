ea3c08e6174bb09b02976b36af6dc26392f55d79
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-04-16 23:12:41 +0300

    1. Edit routes to fit user login. 2. Add user authorization through JWT in ther users controller and application controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,40 +1,63 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only:[:create]
    
-    def index
-        render json: User.all
-    end
+    # def index
+    #     render json: User.all
+    # end
 
+    # def create
+    #     user = User.create(user_params)
+    #     if user
+    #         render json: user, status: :created
+    #     else
+    #         render json: {error: "User not created"}, status: :unprocessable_entity
+    #     end
+    # end
+
+    # def show
+    #     user = User.find_by(id: params[:id])
+    #     if user
+    #         render json: user, status: :ok
+    #     else
+    #         render json: {error:"User not found"}, status: :not_found
+    #     end
+    # end
+
+    # def destroy
+    #     user = User.find_by(id:params[:id])
+    #     if user
+    #         user.destroy
+    #         head :no_content
+    #     else
+    #         render json:{error: "User not found"}, status: :not_found
+    #     end
+    # end
+
+    # auto-login /GET /me
+  def show
+    render json: { user: UserSerializer.new(current_user) }, status: :accepted
+  end
+
+ # POST /sign up
     def create
-        user = User.create(user_params)
-        if user
-            render json: user, status: :created
+        @user = User.create(user_params)
+        if @user.valid?
+            render json: { user: UserSerializer.new(@user) }, status: :created
         else
-            render json: {error: "User not created"}, status: :unprocessable_entity
+            render json: { error: 'failed to create user' }, status: :unprocessable_entity
         end
     end
-
-    def show
-        user = User.find_by(id: params[:id])
-        if user
-            render json: user, status: :ok
-        else
-            render json: {error:"User not found"}, status: :not_found
-        end
+    
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
     end
 
-    def destroy
-        user = User.find_by(id:params[:id])
-        if user
-            user.destroy
-            head :no_content
-        else
-            render json:{error: "User not found"}, status: :not_found
-        end
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
 
-private
 
-def user_params
-    params.permit(:username, :password, :email)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   ##USERS
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  post '/users', to: 'users#create'
+  
+  # get '/users/:id', to: 'users#show'
+  post '/signup', to: 'users#create'
   delete '/users', to: 'users#destroy'
+  get '/me', to: 'users#show'
 
   ##PRODUCTS
   get '/products', to: 'products#index'
