07145cd82cc6a2147f07ee7e12ab99e64b11a00b
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-22 14:19:50 +0300

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    skip_before_action :authorized, only: [:index,:show]
+    skip_before_action :authorized, only: [:index,:show, :latest_products]
 
     # GET /products
     def index
