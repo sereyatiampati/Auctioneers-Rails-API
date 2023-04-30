f5c32a2f3fb3ab73c21709cc0237f8a895b638cc
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-04-30 11:32:50 +0300

    - Add a count feature as a custom method in the Products Serializer. - Also add a bid to the seed data. - Edit the closebid patch method to accept an end_date from the frontend.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,13 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :start_date, :end_date, :starting_price, :image, :category_id
+  attributes :id, :name, :description, :start_date, :end_date, :starting_price, :image, :category_id, :count
 
   has_many :bids
   belongs_to :seller
   belongs_to :category
+
+  def count
+    allbids = self.object.bids.all
+    bidscount = allbids.count
+    "#{bidscount}"
+  end
 end
